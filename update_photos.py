a296b6a957c2d7c3bcd8d430c9b9152cf091a5a0
John Correira <john.correira@gmail.com>
2018-05-02 11:35:05 -0400

    working CR2 raw file organization

--- update_photos.py
+++ update_photos.py
@@ -10,6 +10,17 @@ import subprocess
 
 pidfile = "/tmp/update_photos.pid"
 
+def mkdir_p(dirname):
+
+   if not os.path.exists(dirname):
+      try:
+         os.makedirs(dirname)
+      except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+           pass
+        else:
+           raise
+
 def check_if_running():
 
    if os.path.isfile(pidfile):
@@ -37,54 +48,56 @@ def organize_photos():
 
    # move JPG files to temp and rename with time stamp
    for d in [pictmpdir, rawtmpdir]:
-      if not os.path.exists(d):
-         try:
-            os.makedirs(d)
-         except OSError as exc:  # Python >2.5
-           if exc.errno == errno.EEXIST and os.path.isdir(path):
-              pass
-           else:
-              raise
+      mkdir_p(d)
 
    for dirpath, dirnames, filenames in os.walk(searchpath):
       # if there are no filenames there's nothing to do
       print(dirpath)
+
       if filenames:
          # ignore files in thumbnail and recycle dirs
          parentdir = dirpath.split('/')[-1]
          if parentdir not in skipdirs:
+
             # find jpg, case insensitive
             for filename in fnmatch.filter(filenames, '*.[Jj][Pp][Gg]'):
+
                fullname = os.path.join(dirpath,filename)
                # copy to tmp dir
                shutil.copy(fullname, pictmpdir)  
+
                # if there is a matching CR2 raw file, move that as well
+               # assumes the raw file is in the same directory as jpj file
                # This if for Canon Raw files, if ever get new model camera
                # would have to update extension
                for ext in ['.CR2','.cr2'] :
-                  try:
-                     # replace jpg extension with CR2 extension and try to copy
-                     stripext = fullname.split('.')[:-1]
-                     newfull = '.'.join(stripext)+ext
-                     shutil.copy(newfull, rawtmpdir)  
-                  except:
-                     pass
+                  stripext = fullname.split('.')[:-1]
+                  newfull = '.'.join(stripext)+ext
+                  if os.path.isfile(newfull):
+                     basename = '.'.join(stripext).split('/')[-1]
+                     shutil.copyfile(newfull, rawtmpdir+'/'+basename+'.CR2')  
                   
    # use jhead to move to date specific directory
    # cmd can't be a list, and must use shell=True
    jheadcmd = 'jhead -autorot -n'+picsavdir+'/%Y/%m/%f '+pictmpdir+'/*'
    subprocess.call(jheadcmd, shell=True)
 
-   
+   # use exiftool to get created date for year/month directory structure
+   out = subprocess.check_output('exiftool -createdate '+rawtmpdir+'/*.CR2', shell=True).split('\n')[:-2]
+
+   names = [ f.split(' ')[-1] for f in out[0::2] ]
+   year =  [ f.split(':')[1].strip() for f in out[1::2] ]
+   month = [ f.split(':')[2].strip() for f in out[1::2] ]
+
+   for i in range(0,len(names)):
+     d = '/'.join([picsavdir,year[i],month[i],'raw'])
+     mkdir_p(d)
+     shutil.copy(names[i], d)
+
    #Now any movie files
+
+
    
-   #
-   #if [ $? -eq 0 ]
-   #then
-   #   rm $PICTMPDIR/*
-   #   rm $RAWTMPDIR/*
-   #fi
-   #rsync -vri --backup --include=*.jpg $/ /home/mandrake/Pictures/
 
 def main(argv=None):
 
