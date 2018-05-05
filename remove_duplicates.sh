fbecaa3de08219e2b31729b00794ac11f21c6db7
John Correira <john.correira@gmail.com>
2018-05-04 22:06:14 -0400

    add sed command to remove empty lines

--- remove_duplicates.sh
+++ remove_duplicates.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #https://unix.stackexchange.com/a/383107
-fdupes -r -f -o name //share/pictures/ /share/to_be_organized ...  >/share/home/john/listtodel
+fdupes -r -f -o name //share/pictures/ /share/to_be_organized  >/share/homes/john/listtodel
+
+sed -i '/^\s*$/d' listtodel
